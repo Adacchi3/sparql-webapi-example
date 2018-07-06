d12d76b8be03a503fe99e4b93e0d5b0a19e7bf5f
Adacchi3 <adachi.prog@gmail.com>
2018-07-06 18:56:52 +0900

    [feat] add the model of judge in LatLngController

--- app/controllers/LatLngController.java
+++ app/controllers/LatLngController.java
@@ -5,6 +5,13 @@ import utils.QueryExecutor;
 import play.libs.Json;
 import com.fasterxml.jackson.databind.JsonNode;
 
+class Judge {
+  public float lat;
+  public float lng;
+  public Boolean good;
+  public String reason;
+}
+
 public class LatLngController extends Controller {
   public Result distinguish(float lat, float lng){
     Judge judge = new Judge();
